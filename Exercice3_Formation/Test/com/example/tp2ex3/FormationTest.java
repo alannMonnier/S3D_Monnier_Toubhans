ccf7457b48a87da5a554e8487fbbc38d4d398602
fabien.toubhans7@etu.univ-lorraine.fr <fabien.toubhans7@etu.univ-lorraine.fr>
2023-09-26 16:41:53 +0200

    Ajout de FormationTest

--- Exercice3_Formation/Test/com/example/tp2ex3/FormationTest.java
+++ Exercice3_Formation/Test/com/example/tp2ex3/FormationTest.java
@@ -6,10 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FormationTest {
 
+    public Formation formation1;
+    public Formation formation2;
+
     @BeforeEach
     public void init(){
-        Formation formation1 = new Formation(1);
-        Formation formation2 = new Formation(2);
+        formation1 = new Formation(1);
+        formation2 = new Formation(2);
         formation1.ajouterMatiere("Qualite dev", 12);
         formation1.ajouterMatiere("Anglais", 10);
         formation1.ajouterMatiere("Developpement web", 8);
@@ -21,10 +24,19 @@ class FormationTest {
     @org.junit.jupiter.api.Test
     void ajouterMatiere() {
         assertEquals(formation1.getID(), 1);
+        assertEquals(formation1.getMatieres().get("Anglais"), 10);
+        assertEquals(formation1.getMatieres().get("Qualite dev"), 12);
     }
 
     @org.junit.jupiter.api.Test
     void supprimerMatiere() {
+        assertEquals(formation1.getMatieres().get("Anglais"), 10);
+        formation1.supprimerMatiere("Anglais");
+        assertEquals(formation1.getMatieres().get("Anglais"), null);
+
+        assertEquals(formation2.getMatieres().get("Reseau"), 9);
+        formation2.supprimerMatiere("Reseau");
+        assertEquals(formation2.getMatieres().get("Reseau"), null);
     }
 
     @org.junit.jupiter.api.Test
